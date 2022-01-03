8b63a63f69d3403713f6836b63f339b3b1c815bc
Office PC <72garmartin@gmail.com>
2022-01-03 15:24:17 +0700

    fix error can't get the token instead the object access token, just change the import Laravel\Sanctum\ to Laravel\Passport

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,13 +5,45 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     
 
-    public function login() {
-        echo "Login endpoint requested";
+    public function login(Request $request) {
+        
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid email or password'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        // $token = Http::asForm()->post('http://passport-app.com/oauth/token', [
+        //     'grant_type' => 'password',
+        //     'client_id' => 'client-id',
+        //     'client_secret' => 'client-secret',
+        //     'username' => $request->email,
+        //     'password' => $request->password,
+        //     'scope' => '',
+        // ]);
+
+        $token = $user->createToken('Access Token');
+
+        $user->access_token = $token->accessToken;
+
+        return response()->json([
+            'user' => $user
+        ], 200);
     }
 
     public function signup(Request $request) {
